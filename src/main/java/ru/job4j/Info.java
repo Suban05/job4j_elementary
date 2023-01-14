555a2a3c3a715a4c022004c794016742d492a50b
Anatoly Busygin <anatolyb94@gmail.com>
2023-01-14 20:01:31 +0500

    3.5 Фиксация изменений на github

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -1,8 +1,5 @@
 package ru.job4j;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 public class Info {
     public static void main(String[] args) {
         System.out.println("14.01.2023");
