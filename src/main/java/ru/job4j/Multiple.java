ee34e4c88d320ae278fde9244acd4f2cb47a1abf
Anatoly Busygin <anatolyb94@gmail.com>
2023-01-15 10:35:24 +0500

    4.1. Разбор первой программы

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -2,13 +2,11 @@ package ru.job4j;
 
 public class Multiple {
     public static void main(String[] args) {
-
         int c = 1;
         for (int i = 1; i <= 9; i++) {
             var result = c * i;
             var expression = c + " * " + i + " = " + result;
             System.out.println(expression);
         }
-
     }
 }
