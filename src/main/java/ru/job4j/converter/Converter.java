5e947469b1d7101074ca26f339e222061a74cbc2
Anatoly Busygin <anatolyb94@gmail.com>
2023-01-17 00:33:08 +0500

    Что такое тестирование [#159431]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -10,11 +10,22 @@ public class Converter {
         return value / 60;
     }
 
+    private static void checkConvertingRubToForeignCurrency(float in, float expected, float out) {
+        boolean passed = expected == out;
+        System.out.println(in + " rubles are " + expected + ". Test result : " + passed);
+    }
+
     public static void main(String[] args) {
         int rub = 140;
         float euro = Converter.rubleToEuro(rub);
         float dollar = Converter.rubleToDollar(rub);
         System.out.println(rub + " rubles are " + euro + " euro.");
         System.out.println(rub + " rubles are " + dollar + " dollar.");
+        float in = 140;
+        float out = Converter.rubleToEuro(in);
+        checkConvertingRubToForeignCurrency(in, 2, out);
+        float expectedDollar = 2.3333333F;
+        out = Converter.rubleToDollar(in);
+        checkConvertingRubToForeignCurrency(in, expectedDollar, out);
     }
 }
