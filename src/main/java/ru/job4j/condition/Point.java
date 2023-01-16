7d1797143fac34f2ed52424df661284b69272710
Anatoly Busygin <anatolyb94@gmail.com>
2023-01-16 13:02:49 +0500

    Формула вычисления расстояния между двумя точками в системе координат записана в одну строку

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -4,12 +4,8 @@ import ru.job4j.calculate.Calculator;
 
 public class Point {
     public static double distance(int x1, int y1, int x2, int y2) {
-        int diff1 = Calculator.diff(x1, x2);
-        int diff2 = Calculator.diff(y1, y2);
         byte degree = 2;
-        double arg1 = Math.pow(diff1, degree);
-        double arg2 = Math.pow(diff2, degree);
-        return Math.sqrt(arg1 + arg2);
+        return Math.sqrt(Math.pow((x2 - x1), degree) + Math.pow((y2 - y1), degree));
     }
 
     public static void main(String[] args) {
