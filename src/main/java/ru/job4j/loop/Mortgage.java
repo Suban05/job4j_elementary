467966e930c4a6ddb68e8772a3d03a8f2729df68
Anatoly Busygin <anatolyb94@gmail.com>
2023-01-24 19:55:00 +0500

    Расчет ипотеки помещен внутрь цикла

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,9 +3,8 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-        double start = amount + amount * percent / 100;
-        while (start > 0) {
-            start -= salary;
+        while (amount > 0) {
+            amount = (amount + amount * percent / 100) - salary;
             year++;
         }
         return year;
