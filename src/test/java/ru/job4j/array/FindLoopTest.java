833ecb699c0f233a40077767457c3876faa831bc
Anatoly Busygin <anatolyb94@gmail.com>
2023-01-25 20:41:01 +0500

    Классический поиск перебором. [#33489] Изменено имя тест-метода: SquareTest.whenBound5Then -> SquareTest.whenBound5Then014916

---
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FindLoopTest {
+    @Test
+    public void whenArrayHas5Then0() {
+        int[] data = new int[] {5, 10, 3};
+        int el = 5;
+        int result = FindLoop.indexOf(data, el);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenArrayHasNot10ThenMinus1() {
+        int[] data = new int[] {11, 16, 54};
+        int el = 10;
+        int result = FindLoop.indexOf(data, el);
+        int expected = -1;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenArrayHas34Then1() {
+        int[] data = new int[] {0, 34, 13};
+        int el = 34;
+        int result = FindLoop.indexOf(data, el);
+        int expected = 1;
+        assertThat(result).isEqualTo(expected);
+    }
+}
