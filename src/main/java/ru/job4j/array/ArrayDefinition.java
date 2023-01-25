90b5bde4b0da6fe27841e943cf59d1e52e0c64fb
Anatoly Busygin <anatolyb94@gmail.com>
2023-01-25 19:55:36 +0500

    Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -1,6 +1,21 @@
 package ru.job4j.array;
 
 public class ArrayDefinition {
+    public static String[] names() {
+        String[] names = new String[4];
+        names[0] = "Bob";
+        names[1] = "Poul";
+        names[2] = "Mark";
+        names[3] = "Mike";
+        return names;
+    }
+
+    public static void messageNames(String[] names) {
+        for (String name:
+                names) {
+            System.out.println(name);
+        }
+    }
 
     public static void main(String[] args) {
         short[] ages = new short[10];
@@ -9,5 +24,7 @@ public class ArrayDefinition {
         System.out.println(surnames.length);
         float[] prices = new float[40];
         System.out.println(prices.length);
+        String[] names = names();
+        messageNames(names);
     }
 }
