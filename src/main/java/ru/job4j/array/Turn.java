0f9c169e26aa76d8dbb0844611f0ce7ff24d0162
Anatoly Busygin <anatolyb94@gmail.com>
2023-01-26 19:57:25 +0500

    Упрощение метода Turn.back

--- src/main/java/ru/job4j/array/Turn.java
+++ src/main/java/ru/job4j/array/Turn.java
@@ -2,15 +2,8 @@ package ru.job4j.array;
 
 public class Turn {
     public static int[] back(int[] array) {
-        int j = array.length - 1;
-        int count = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (count >= array.length) {
-                break;
-            }
-            SwitchArray.swap(array, i, j);
-            j--;
-            count += 2;
+        for (int i = 0; i < array.length / 2; i++) {
+            SwitchArray.swap(array, i, array.length - i - 1);
         }
         return array;
     }
