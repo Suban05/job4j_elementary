22eda5947bb62093d1f3c8f309d8b6b83fdedd19
Anatoly Busygin <anatolyb94@gmail.com>
2023-01-27 09:26:31 +0500

    Удалена лишняя переменная в RollBackArray.rollback

--- src/test/java/ru/job4j/array/RollBackArray.java
+++ src/test/java/ru/job4j/array/RollBackArray.java
@@ -4,8 +4,7 @@ public class RollBackArray {
     public static int[] rollback(int[] array) {
         int[] result = new int[array.length];
         for (int index = 0; index < array.length; index++) {
-            int lastIndex = array.length - index - 1;
-            result[index] = array[lastIndex];
+            result[index] = array[array.length - index - 1];
         }
         return result;
     }
