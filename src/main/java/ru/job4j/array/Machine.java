f6f29ee6b371aa2425138fcee108928a3385474e
Anatoly Busygin <anatolyb94@gmail.com>
2023-01-28 15:22:28 +0500

    fori -> foreach в Machine.change

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -8,10 +8,10 @@ public class Machine {
         int[] rsl = new int[100];
         int size = 0;
         int need = money - price;
-        for (int i = 0; i < coins.length; i++) {
-            while (need >= coins[i]) {
-                rsl[size] = coins[i];
-                need -= coins[i];
+        for (int coin : coins) {
+            while (need >= coin) {
+                rsl[size] = coin;
+                need -= coin;
                 size++;
             }
         }
